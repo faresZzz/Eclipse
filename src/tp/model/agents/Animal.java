a6eedd1760260f2be8b7cfc30958e3ee3b57177a
Fares <fares.zaghouane@cpe.fr>
2021-11-23 17:43:48 +0100

    TP2 debut partie 6

--- src/tp/model/agents/Animal.java
+++ src/tp/model/agents/Animal.java
@@ -19,7 +19,7 @@ public class Animal {
 	/** position sur la carte*/
 	private Point coord;
 	/** état de santé de l'animal */
-	private Etat etat=Etat.Normal;
+	private Etat etat;
 	private Sexe sexe;
 	
 	/* 
@@ -30,6 +30,7 @@ public class Animal {
 		age = 0;
 		id = Animal.getUniqueId();
 		this.sexe=sexe;
+		this.etat = Etat.Normal;
 		//this.coord=coord;
 		this.coord=new Point(coord);
 	}
@@ -40,6 +41,9 @@ public class Animal {
 		 * une bonne manière de faire 
 		 * en utilisant ce qui existe déjà, une moins bonne
 		 */
+		
+		this(sexe, new Point(0,0));
+	
 	}
 	
 	public Animal() {
@@ -48,14 +52,46 @@ public class Animal {
 		 * une bonne manière de faire 
 		 * en utilisant ce qui existe déjà, une moins bonne
 		 */
+		
+	 this(Sexe.Femelle);
 	}
 	
 	/*
 	 *  Accesseurs et mutateurs
 	 */
 	//TODO
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age  > this.age)
+		{
+			this.age = age;
+		}
+		
+	}
+
+	public Point getCoord() {
+		return (Point)this.coord.clone();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Etat getEtat() {
+		return etat;
+	}
+
+	public Sexe getSexe() {
+		return sexe;
+	}
 	
-	
+	public String toString() {
+		return  this.getClass().getSimpleName() + " n°" + this.id + "(sexe=" + this.sexe + "( position x: " + this.coord.x + "; position y: " + this.coord.y+ "))";
+	}
+
 	/*
 	 * Redéfinitions de méthodes d'object
 	 */
@@ -68,13 +104,56 @@ public class Animal {
 	
 	
 
+	private int ecart(double offset) 
+	{
+		int valRen;
+		if (offset < 0.33)
+		{
+			valRen = -1;
+		}
+		else if(offset < 0.66)
+		{
+			valRen = 0;
+		}
+		else
+		{
+			valRen = 1;
+		}
+		return valRen;
+	}
+
 	public void seDeplacer() {
 		//TODO utiliser Math.random() pour choisir une direction de déplacement
+		double valx = Math.random();
+		double valy = Math.random();
+		
+		int dx = ecart(valx);
+		int dy = ecart(valy);
+		this.coord.translate(dx, dy);
+		
 	}
 	
 	public void vieillir() {
 		//TODO fait vieillir l'animal d'une unité de temps
 		//une bonne manière de faire, une moins bonne...
+		this.setAge(this.age ++);
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (age != other.age)
+			return false;
+		if (etat != other.etat)
+			return false;
+		if (sexe != other.sexe)
+			return false;
+		return true;
 	}
 	
 	public void rencontrer(Animal a) {
@@ -88,7 +167,8 @@ public class Animal {
 	 */
 	private static int getUniqueId() {
 		//TODO 
-		return 0;
+		currentId ++ ;
+		return currentId;
 	}
 	
 	public static void main(String args[]) {
@@ -113,41 +193,67 @@ public class Animal {
 		System.out.println(d);
 		
 		System.out.println("*** Getters et setters **********");
-		/*
+		
 		System.out.println(d.getSexe());
 		Sexe ss = d.getSexe();
 		ss=Sexe.Male;
 		System.out.println(d.getSexe());
-		*/
+		
+
+		System.out.println(d.getAge());
+		int age = d.getAge();
+		age ++ ;
+		System.out.println(d.getAge());
+		
+
+		
+		
 		//les lignes suivantes devraient afficher la même chose....
-		/*
+		
 		System.out.println(d.getCoord());
 		Point pt = d.getCoord();
 		pt.x=50;
 		System.out.println(d.getCoord());
-		*/
+		
 		
 		//TODO ajoutez vos propres tests de getters et setters
 		
-		//TODO test vieillir
+//		//TODO test vieillir
+//		System.out.println("*** Test vieillir****************");
+//		System.out.println(d.getAge());
+//		d.vieillir();
+//		System.out.println(d.getAge());
+//		
+//		//TODO test seDeplacer
+//		System.out.println("*** Test seDeplacer****************");
+//		System.out.println(d.getCoord());
+//		d.seDeplacer();;
+//		System.out.println(d.getCoord());
+//		
+//		//TODO test id
+//		System.out.println("*** Test id****************");
+//		System.out.println(a.getId());
+//		System.out.println(b.getId());
+//		System.out.println(d.getId());
+//		System.out.println(e.getId());
 		
-		//TODO test seDeplacer
-		
-		//TODO test id
-		/*
-		System.out.println(a.getId());
-		System.out.println(b.getId());
-		*/
 		
 		/*
 		 * Test comparaison
 		 */
-		/*
+		
 		System.out.println(d==e);
 		System.out.println(d.equals(e));
 		System.out.println("Bonjour"=="Bonjour");
 		System.out.println("Bonjour".equals("Bonjour"));
-		*/
+		
 	}
 
+	
+
+	
+	
+
+	
+	
 }
